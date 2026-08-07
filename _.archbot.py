4972fbcdb36abd4a7d3963309855c237f065e06c
martin88699-bit <martin88699@gmail.com>
2026-08-07 20:44:34 +0800

    Add files via upload

---
+++ _.archbot.py
@@ -0,0 +1,14 @@
+import os
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, CommandHandler, ContextTypes
+TOKEN=os.environ["BOT_TOKEN"]
+async def start(update:Update, context:ContextTypes.DEFAULT_TYPE):
+    kb=[
+      [InlineKeyboardButton("🎮 开始游戏",url="https://h5.ddwin01.com/#/Home")],
+      [InlineKeyboardButton("📝 注册账号",url="https://go.crisp.chat/chat/embed/?website_id=029a087e-7f0f-4034-a119-fdef568c3105")],
+      [InlineKeyboardButton("📞 联系客服",url="https://wa.me/601175766643")]
+    ]
+    await update.message.reply_text("欢迎来到 S22 ENTERTAINMENT CITY",reply_markup=InlineKeyboardMarkup(kb))
+app=Application.builder().token(TOKEN).build()
+app.add_handler(CommandHandler("start",start))
+app.run_polling()
